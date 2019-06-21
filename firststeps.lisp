ca0c3053842e554a942440883c994519003a72d3
Leo <51866565+leonardodominik801@users.noreply.github.com>
2019-06-21 10:41:55 -0400

    Update firststeps.lisp

--- firststeps.lisp
+++ firststeps.lisp
@@ -1,13 +1,13 @@
 ;; firststeps.lisp
 
-(format t "Hello, world!")
-
-;; Single line comments start with a semicolon, and can start at any point in the line
-
 #|
-  This is a multi-line comment.
+  Lisp syntax is very simple: there are few rules to remember. 
 
-  #|
-    They can be nested!
-  |#
+  Syntax is made up of S-expressions. An S-expression is either an atom or a list.
+
+  Atoms can be numbers like 10, 3.14, or symbols like t (the truth constant), +, my-variable. 
+  There’s also a special kind of symbol called keywords, which are colon-prefixed symbols like :thing or :keyword. 
+  Keywords evaluate to themselves: you can think of them sort of like enums.
 |#
+
+(format t "Hello, world!")
