a1bb757b9abf3cef82ac72419be7cd4de55e455f
Leo <51866565+leonardodominik801@users.noreply.github.com>
2019-06-21 10:39:57 -0400

    Create firststeps.lisp

---
+++ firststeps.lisp
@@ -0,0 +1,13 @@
+;; firststeps.lisp
+
+(format t "Hello, world!")
+
+;; Single line comments start with a semicolon, and can start at any point in the line
+
+#|
+  This is a multi-line comment.
+
+  #|
+    They can be nested!
+  |#
+|#
