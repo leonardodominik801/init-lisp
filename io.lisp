83aee1b8f2b2fa589c5ea02f2f34675d2afcacc6
Leo <51866565+leonardodominik801@users.noreply.github.com>
2019-06-25 10:58:07 -0400

    io lisp

---
+++ io.lisp
@@ -0,0 +1,9 @@
+;; io.lisp: https://lisp-lang.org/learn/io
+
+;; format
+;; ~~~~~~
+;; The most common way to print data to the screen is using the format 
+;; function. This is like C’s printf, but embedding a whole language for 
+;; printing. (see http://www.gigamonkeys.com/book/a-few-format-recipes.html)
+
+
